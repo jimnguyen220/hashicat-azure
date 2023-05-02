46009a9ef8234ae2f525b5916a66dcbb95344895
jimnguyen220 <jimnguyen220@gmail.com>
2023-05-02 19:40:50 +0000

    Added the second tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "hashicat-for-azure-wf"
     workspaces {
       name = "hashicat-azure"
